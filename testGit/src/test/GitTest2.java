050377db892dcd2166fb84bf45c03cb890ac1714
tanukei0416 <keijirou.tanuma0416@gmail.com>
2021-08-02 20:50:33 +0900

    新規ファイル2の追加

---
+++ testGit/src/test/GitTest2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Github2!");
+		System.out.println("Githubに接続成功2！");
+		System.out.println("Githubからの更新2！");
+	}
+
+}
